c3385a3c1772f5bfc887635dbdd5c75d09ae8720
Jan Seifert <jan.seifert@fotkyzcest.net>
2016-07-06 20:47:56 +0200

    enhance metadata in setup.py

--- setup.py
+++ setup.py
@@ -21,23 +21,40 @@ class PyTest(TestCommand):
         errno = pytest.main(self.pytest_args)
         sys.exit(errno)
 
+description = (
+    "Simple Python's Tornado wrapper which provides helpers for creation"
+    "a new project, writing management commands, service processes, ..."
+)
+
+try:
+    if sys.version_info >= (3,):
+        long_description = open('README.rst', 'rb').read().decode('utf-8')
+    else:
+        long_description = open('README.rst', 'r').read().decode('utf-8')
+except IOError:
+        long_description = description
 
 setup(
     name="shelter",
     version='1.0.0',
-    author='Seznam.cz, a.s.',
-    author_email="advert-vyvoj@firma.seznam.cz",
-    description=(
-        "Simple Python's Tornado wrapper which provides helpers for creating"
-        "a new project, writing management commands, service processes, ..."
-    ),
+    author='Jan Seifert (Seznam.cz, a.s.)',
+    author_email="jan.seifert@firma.seznam.cz",
+    description=description,
+    long_description=long_description,
     license="BSD",
-    url='https://github.com/seznam/shelter',
+    url='https://pypi.python.org/pypi/shelter/',
     classifiers=[
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries :: Python Modules',
     ],
+    platforms=['any'],
     packages=find_packages(include=['shelter*']),
     include_package_data=True,
     zip_safe=False,
