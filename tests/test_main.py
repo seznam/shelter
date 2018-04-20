dfcf30be9a5e6fdbc8ce90c79f022a599f00df2d
Jan Seifert <jan.seifert@firma.seznam.cz>
2018-04-20 09:09:16 +0200

    main module tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -33,8 +33,10 @@ class AppConfigBadType(object):
 
 @mock.patch.object(BaseCommand, 'command')
 def test_main(m):
-    shelter.main.main(['-s', 'tests.settings1', 'testcmd'])
+    with pytest.raises(SystemExit) as e:
+        shelter.main.main(['-s', 'tests.settings1', 'testcmd'])
     m.assert_called_once_with()
+    assert e.value.code == 0
 
 
 def test_main_exit_when_no_settings_module():
@@ -55,7 +57,9 @@ def test_main_default_config_class():
         assert type(self.context.config) == Config
 
     with mock.patch.object(BaseCommand, 'command', new=command):
-        shelter.main.main(['-s', 'tests.settings1', 'testcmd'])
+        with pytest.raises(SystemExit) as e:
+            shelter.main.main(['-s', 'tests.settings1', 'testcmd'])
+        assert e.value.code == 0
 
 
 def test_main_default_config_class_when_settings_is_not_required():
@@ -64,7 +68,9 @@ def test_main_default_config_class_when_settings_is_not_required():
         assert type(self.context.config) == Config
 
     with mock.patch.object(StartProject, 'command', new=command):
-        shelter.main.main(['startproject', 'foo'])
+        with pytest.raises(SystemExit) as e:
+            shelter.main.main(['startproject', 'foo'])
+        assert e.value.code == 0
 
 
 def test_main_custom_config_class():
@@ -73,7 +79,9 @@ def test_main_custom_config_class():
         assert type(self.context.config) == AppConfig
 
     with mock.patch.object(BaseCommand, 'command', new=command):
-        shelter.main.main(['-s', 'tests.settings3', 'testcmd'])
+        with pytest.raises(SystemExit) as e:
+            shelter.main.main(['-s', 'tests.settings3', 'testcmd'])
+        assert e.value.code == 0
 
 
 def test_main_custom_config_class_bad_type():
@@ -92,3 +100,24 @@ def test_main_exit_when_no_action():
     with pytest.raises(SystemExit) as e:
         shelter.main.main(['-s', 'tests.settings1'])
     assert e.value.code == 2
+
+
+@mock.patch.object(BaseCommand, 'command')
+def test_main_fail_when_command_error(m):
+    m.side_effect = [KeyError]
+    with pytest.raises(SystemExit) as e:
+        shelter.main.main(['-s', 'tests.settings1', 'testcmd'])
+    m.assert_called_once_with()
+    assert e.value.code == 1
+
+
+@mock.patch.object(BaseCommand, 'command')
+def test_main_force_exit_when_children(m_command):
+    m_command.side_effect = [KeyError]
+    with mock.patch('shelter.main.multiprocessing') as m_multiprocessing:
+        m_multiprocessing.active_children.return_value = [mock.Mock()]
+        with mock.patch('os._exit') as m_os_exit:
+            m_os_exit.side_effect = [SystemExit]
+            with pytest.raises(SystemExit):
+                shelter.main.main(['-s', 'tests.settings1', 'testcmd'])
+    m_os_exit.assert_called_once_with(1)
