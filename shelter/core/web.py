1aa0c1340c57ec6d6a2f67a9287f2fe067ea6699
Jan Seifert <jan.seifert@firma.seznam.cz>
2018-11-05 14:52:23 +0100

    refactor - passing context and interface into request handler

--- shelter/core/web.py
+++ shelter/core/web.py
@@ -11,17 +11,20 @@ __all__ = ['BaseRequestHandler']
 
 class BaseRequestHandler(RequestHandler):
     """
-    Base class for HTTP handlers, overrides
-    :class:`tornado.web import RequestHandler`. Adds *logger* attribute
-    with Python's logger instance, *context* with
-    :class:`shelter.core.context.Context` instance and *interface* with
-    :class:`shelter.core.config.Config.Interface` instance with definition
-    of the interface.
+    Base class for handlers, :class:`tornado.web import RequestHandler`
+    descendant. Adds *logger* attribute with Python's logger instance,
+    *context* with :class:`shelter.core.context.Context` instance and
+    *interface* with :class:`shelter.core.config.Config.Interface`
+    instance.
     """
 
-    def initialize(self, **kwargs):
-        self.context = kwargs['context']
-        self.interface = kwargs['interface']
+    def __init__(self, application, request, **kwargs):
+        self.context = application.settings['context']
+        self.interface = application.settings['interface']
+
+        super(BaseRequestHandler, self).__init__(
+            application, request, **kwargs)
+
         self.logger = logging.getLogger(
             "{:s}.{:s}".format(__name__, self.__class__.__name__))
 
@@ -31,6 +34,6 @@ class NullHandler(BaseRequestHandler):
     HTTP Handler for '/' when not defined any url to handler route.
     """
 
-    def get(self):
+    def get(self, *unused_args, **unused_kwargs):
         self.write("Interface '%s' works!\n" % self.interface.name)
         self.set_header("Content-Type", 'text/plain; charset=UTF-8')
--- tests/test_core_web.py
+++ tests/test_core_web.py
@@ -1,31 +1,35 @@
 
 import mock
 
+import tornado.web
+
 from shelter.core.config import Config
 from shelter.core.web import BaseRequestHandler, NullHandler
 
 
 def test_base_request_handler():
-    with mock.patch.object(BaseRequestHandler, '__init__', return_value=None):
-        h = BaseRequestHandler()
-        h.initialize(context=1, interface=2)
+    app = tornado.web.Application([], context=1, interface=2)
+    req = mock.Mock()
+
+    h = BaseRequestHandler(app, req)
+
     assert h.context == 1
     assert h.interface == 2
 
 
 def test_null_handler():
-    with mock.patch.object(NullHandler, '__init__', return_value=None), \
-         mock.patch.object(NullHandler, 'write') as m_write, \
-         mock.patch.object(NullHandler, 'set_header') as m_set_header:
-        h = NullHandler()
-        h.initialize(
-            context=object(),
-            interface=Config.Interface(
-                name='foo', host='', port=1, unix_socket=None,
-                processes=0, urls=[]
-            )
+    app = tornado.web.Application(
+        [], context=object(), interface=Config.Interface(
+            name='foo', host='', port=1, unix_socket=None,
+            processes=0, urls=[]
         )
-        h.get()
+    )
+    req = mock.Mock()
+
+    with mock.patch.object(NullHandler, 'write') as m_write:
+        with mock.patch.object(NullHandler, 'set_header') as m_set_header:
+            h = NullHandler(app, req)
+            h.get()
 
     m_write.assert_called_once_with("Interface 'foo' works!\n")
     m_set_header.assert_called_once_with(
